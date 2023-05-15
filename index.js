21cae314c4457c056e53a88937bc6e8884932f99
anothersam22 <stam84@my.bcit.ca>
2023-05-15 13:19:58 -0700

    add difficulty level buttons

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
 const setup = () => {
-  let firstCard = undefined;
-  let secondCard = undefined;
+  let firstCard = null;
+  let secondCard = null;
+  let isProcessing = false;
 
-  $(".card").on("click", function () {
+  $(document).on("click", ".card", function () {
+    if (isProcessing) {
+      return;
+    }
+
     $(this).toggleClass("flip");
 
+    const currentCard = $(this).find(".front_face")[0];
+
     if (!firstCard) {
-      firstCard = $(this).find(".front_face")[0];
-    } else {
-      secondCard = $(this).find(".front_face")[0];
-      console.log(firstCard, secondCard);
+      firstCard = currentCard;
+    } else if (firstCard !== currentCard) {
+      secondCard = currentCard;
+      isProcessing = true;
+
       if (firstCard.src === secondCard.src) {
         console.log("match");
-        $("#match-message").text("Match!"); // Print "Match!" to an element with id "match-message"
+        $("#match-message").text("Match!");
         $(`#${firstCard.id}`).parent().off("click");
         $(`#${secondCard.id}`).parent().off("click");
+        isProcessing = false;
+        firstCard = null;
+        secondCard = null;
       } else {
         console.log("no match");
+        $("#match-message").text("No Match!");
         setTimeout(() => {
-          $(`#${firstCard.id}`).parent().toggleClass("flip");
-          $(`#${secondCard.id}`).parent().toggleClass("flip");
+          if (firstCard && secondCard) {
+            $(`#${firstCard.id}`).parent().toggleClass("flip");
+            $(`#${secondCard.id}`).parent().toggleClass("flip");
+            isProcessing = false;
+            firstCard = null;
+            secondCard = null;
+          }
         }, 1000);
       }
-
-      // Reset variables for the next pair of cards
-      firstCard = undefined;
-      secondCard = undefined;
     }
   });
 };
